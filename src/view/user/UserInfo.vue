320ec8844c9008f9cb69f0384fb73751c9220cf6
Noni <1063030602@qq.com>
2022-12-27 23:13:36 +0800

    feat: 用户端个人信息界面

--- src/view/user/UserInfo.vue
+++ src/view/user/UserInfo.vue
@@ -1,22 +1,57 @@
 <template>
-    <div class="UserInfo">
-        <h1>UserInfo</h1>
-    </div>
+  <div class="UserInfo">
+    <header>
+      <img
+        src="../../assets/Home/user.jpg"
+        style="border-radius: 100%; width: 150px"
+      />
+      <div class="info">
+        <p>姓名<span>noni</span></p>
+        <p>账号<span>1063030602</span></p>
+        
+      </div>
+    </header>
+    <main>
+        
+    </main>
+  </div>
 </template>
 <script>
 export default {
-   data() {
-      return {
-      }
-   },
-   created(){
-   },
-   computed:{
-   },
-   methods:{
-   },
-}
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+};
 </script>
-<style>
-
+<style lang="less" scoped>
+.UserInfo {
+  width: 80vw;
+  //   background-color: #f5f5f5;
+}
+header {
+    margin-left: 80px;
+  img {
+    float: left;
+  }
+  .info {
+    float: left;
+    margin-top: 15px;
+    margin-left: 80px;
+    p {
+      font-weight: 600;
+      margin-top: 10px;
+      font-size: 20px;
+      span {
+      font-weight: 400;
+      margin-top: 10px;
+      padding-left: 20px;
+      font-size: 16px;
+    }
+    }
+    
+  }
+}
 </style>
